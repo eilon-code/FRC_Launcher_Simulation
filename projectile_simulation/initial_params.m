26914745042328f5eea258c800634dcd32cf3170
eilon-code <eilon.halevy12@gmail.com>
2026-01-19 00:36:09 +0200

    updated simulation to use more advanced physical model. also- simplified the simulation input to vector of V_0.

--- projectile_simulation/initial_params.m
+++ projectile_simulation/initial_params.m
@@ -1,10 +1,13 @@
 function initial_params()
-    C_d = 0.5;              % drag coefficient
-    C_l = 0.2;              % lift coefficient
-    rho = 1.225;            % air density kg/m^3
     g = 9.81;               % Earth Gravity facotor
     m = 0.230;              % Mass of projectile
     r = 0.075;              % radius of projectile
+    rho = 1.225;            % air density kg/m^3
+    
+    C_d = 0.4;              % drag coefficient
+    C_l = 0.2;              % lift (base) coefficient
+    k_lift = 1.2;           % lift decay
+    C_m = 0.01;             % Change of omega according to moment tau
 
     % Assign parameters to the base workspace for further calculations
     assignin('base', 'g', g);  % gravitational acceleration m/s^2
@@ -13,10 +16,11 @@ function initial_params()
     assignin('base', 'rho', rho);
     assignin('base', 'C_d', C_d);
     assignin('base', 'C_l', C_l);
+    assignin('base', 'k_lift', k_lift);
+    assignin('base', 'C_m', C_m);
     % --- הוספת משתני דמה כדי שהקומפילציה תעבור ---
-    assignin('base', 'V_mag', 1);
-    assignin('base', 'Angle', 0.1);
     assignin('base', 'Initial_Height', 0);
-    assignin('base', 'omega_val', [0, 0, 0]);
+    assignin('base', 'V_0', [0,0,0]);           % initial velocity
+    assignin('base', 'omega_0', [0, 0, 0]);     % initial (internal) rotation velocity
     % ---------------------------------------------
 end
